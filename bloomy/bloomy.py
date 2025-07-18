5290859a57e51b72e963f31f4542dbd16372e28f
うた。 <utaro.9503@gmail.com>
2025-07-18 15:59:20 +0900

    Subproccess使用時のShutdownエラーの修正

--- bloomy/bloomy.py
+++ bloomy/bloomy.py
@@ -130,8 +130,8 @@ class Bloomy(object):
         log.debug("Connecting to Discord...")
 
         # noinspection PyTypeChecker
-        signal.signal(signal.SIGTERM, lambda *_: self.loop.create_task(self.shutdown()))
-        signal.signal(signal.SIGINT, lambda *_: self.loop.create_task(self.shutdown()))
+        if hasattr(signal, "SIGBREAK"):
+            signal.signal(signal.SIGBREAK, lambda _, __: self.loop.create_task(self.shutdown()))
         asyncio.create_task(self.on_connect())
         await bot.start(token=self.config.token)
 
